8c7e918511f877c19fa6421976cfa48475827cb0
Lyndon R / lyndon025 <lyndongaming@gmail.com>
2026-01-10 21:51:19 +0800

    Fix: Explicitly allowed CORS for Vercel app

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,14 @@ from src.config import Config
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    # Explicitly allow the frontend origin
+    CORS(app, resources={r"/*": {"origins": [
+        "https://spam-detectph.vercel.app",
+        "http://127.0.0.1:5500", 
+        "http://localhost:5500",
+        "http://127.0.0.1:3000",
+        "http://localhost:3000"
+    ]}})
     
     from src.routes import main_bp
     app.register_blueprint(main_bp)
